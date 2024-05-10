dd58b15602a4f9fccc5aafd3d6a863a87f49e14a
Prashnt Sachan <swepra04@gmail.com>
2024-05-10 09:25:30 +0530

    add code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Rating from './components/Rating';
 function App() {
   return (
     <div className="App">
-      <Rating/>
+      <Rating />
     </div>
   );
 }
--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaStar } from "react-icons/fa";
-const rating = ({ noOfStars = 7 }) => {
+import "./Styles.css"
+const Rating = ({ noOfStars = 7 }) => {
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+    function handleClick(index) {
+        setRating(index)
+    }
+    function handleMouseEnter(index) {
+        setHover(index)
+    }
+    function handleMouseLeave(index) {
+        setHover(rating);
+    }
     return (
         <div>
             {[...Array(noOfStars)].map((_, index) => (
-                <FaStar size={35} key={index} />
+                <FaStar
+                    size={25}
+                    key={index}
+                    className={index < (hover || rating) ? 'active' : 'inactiv'}
+                    onClick={() => { handleClick(index + 1) }}
+                    onMouseEnter={() => handleMouseEnter(index + 1)}
+                    onMouseLeave={() => handleMouseLeave(index + 1)} />
             ))}
         </div>
     )
 }
 
-export default rating
+export default Rating
